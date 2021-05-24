350a7d4a59c2793449f89fbca6bb0656c4f7b118
sasikala2668 <s.sasikala24797@gmail.com>
2021-05-24 18:40:53 +0530

    added delete mapping api in controller

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +33,11 @@ public class StudentController {
 	@GetMapping("/getAll")
 	public List<StudentEntity> getAllStudentDetails() {
 		return studentService.getAllStudentDetails();
+	}
+	
+	@DeleteMapping("/deleteById")
+		public StudentEntity deleteById(@PathVariable Long id) {
+		return null;
 	}
 
 }
